6a246224ac09374a17d8bddb6c4ec23cebd84787
Josefine Loroff <josefine.loroff@studium.fernuni-hagen.de>
2018-07-18 21:10:16 +0200

    add server request

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require ("express")
 var app = express()
+var bodyParser = require("body-parser")
 
-
+// tell express to use body-parser
+app.use(bodyParser.urlencoded({extended: true}))
+// tell express to serve the content of the public directory 
+app.use(express.static("public")) 
+// expect all files to be ejs files
+app.set("view engine", "ejs")
 
 
 
 app.get("/", function(req, res) {
-    res.render("home.ejs")
+    res.render("home")
 //    res.send("Hi there, welcome to my assignment!")
 })
 
@@ -18,7 +24,7 @@ app.get("/", function(req, res) {
 
 app.get("/fallinlovewith/:thing", function(req, res) {
     var thing = req.params.thing
-    res.render("love.ejs", {thingVar : thing})
+    res.render("love", {thingVar : thing})
 })
 
 app.get("/speak/:animal", function(req, res) {
@@ -47,11 +53,47 @@ app.get("/speak/:animal", function(req, res) {
  })
 
 
+ app.get("/posts", function(req, res) {
+     var posts = [
+         {title: "Post 1", author: "Susi"},
+         {title: "Post 2", author: "Rudi"},
+         {title: "Post 3", author: "Käthe"},
+
+     ]
+     res.render ("posts", {posts: posts})
+ })
+
+
+ app.post("/addfriend", function(req, res) {
+    var newFriend = req.body.newfriend
+    friends.push(newFriend)
+    console.log(req.body)
+    res.redirect("/friends")
+})
+
+ var friends = [
+    "Rami", "Rowi", "Dani", "Jola", "Babsi"
+]
+
+ app.get("/friends", function(req, res) {  
+     res.render("friends", {friends: friends})
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get("*", function(req, res) {
     res.send("Sorry, Page not found... What are you doing with your life?")
 })
-
-
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("server connected")
